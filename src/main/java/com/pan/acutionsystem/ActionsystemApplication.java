48624053fb13c7de73ab503666afdb933482760d
Amo <774629027@qq.com>
2019-03-10 23:03:22 +0800

    init

---
+++ src/main/java/com/pan/acutionsystem/ActionsystemApplication.java
@@ -0,0 +1,13 @@
+package com.pan.acutionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionsystemApplication.class, args);
+    }
+
+}
